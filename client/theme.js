4b3657ebd5bcaa2eb2cff905ba440f787cc0137b
Bharti <bhartinandan886@gmail.com>
2023-09-08 21:22:12 +0530

    Code tested for normal social sites

---
+++ client/theme.js
@@ -0,0 +1,24 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import { teal, orange } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+      light: '#8e24aa',
+      main: '#6a1b9a',
+      dark: '#4a148c',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ffd95b',
+      main: '#ffa726',
+      dark: '#c77800',
+      contrastText: '#000',
+    },
+      openTitle: teal['700'],
+      protectedTitle: orange['700'],
+      type: 'light'
+    }
+  })
+
+  export default theme  
